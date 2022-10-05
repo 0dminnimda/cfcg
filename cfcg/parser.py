9a127099a14892e13d52005623fa1eab7433f860
0dminnimda <0dminnimda@gmail.com>
2022-10-05 22:56:31 +0300

    parser.py: clean up failed attempts to implement preprocessing

--- cfcg/parser.py
+++ cfcg/parser.py
@@ -1,52 +1,13 @@
-from distutils.ccompiler import CCompiler, new_compiler
 import io
-import os
 from pathlib import Path
-from subprocess import check_output
-from typing import List, Optional
 
-import pycparser
 from pcpp import Preprocessor
-from pycparser import c_ast
-
-
-def fixed_spawn(self: CCompiler, cmd: List[str]) -> None:
-    print(cmd)
-    if self.dry_run:
-        return
-    # check_output(path_list, input=source, universal_newlines=True)
-
-
-def new_fixed_compiler(*args, **kwargs):
-    compiler = new_compiler(*args, **kwargs)
-    type(compiler).spawn = fixed_spawn
-    return compiler
-
+from pycparser import CParser, c_ast
 
 FAKE_LIBC = "pycparser\\utils\\fake_libc_include"
 
 
 def preprocess(source: str, filename: str = "<unknown>") -> str:
-    # if cc is None:
-    #     compiler = new_fixed_compiler(dry_run=1)
-    #     compiler.compile(["<test>.c"], include_dirs=["../utils/fake_libc_include"], extra_postargs=["-E"])
-    #     try:
-    #         cc = [compiler.cc]
-    #     except AttributeError:
-    #         cc = compiler.compiler
-    #     print(cc)
-
-    # exit()
-
-    # path_list = cc + ["-E"]
-
-    # try:
-    #     text = check_output(path_list, input=source, universal_newlines=True)
-    # except OSError as e:
-    #     raise RuntimeError("Unable to invoke 'cpp'.  " +
-    #         'Make sure its path was passed correctly\n' +
-    #         ('Original error: %s' % e))
-
     p = Preprocessor()
     p.add_path(Path(__file__).parent.parent / FAKE_LIBC)
 
@@ -55,19 +16,7 @@ def preprocess(source: str, filename: str = "<unknown>") -> str:
     p.write(output := io.StringIO())
     return output.getvalue()
 
-    # # dirty hack to make pycparser work
-    # source = "\n".join(
-    #     (line for line in source.split("\n") if not line.startswith("#include"))
-    # )
-
-    # return source
-
-
-# class CParser(pycparser.CParser):
-#     def p_pp_directive(self, p):
-#         pass
-
 
 def parse(source: str, filename: str = "<unknown>", **kwargs) -> c_ast.Node:
-    parser = pycparser.CParser(**kwargs)
+    parser = CParser(**kwargs)
     return parser.parse(preprocess(source, filename), filename)
